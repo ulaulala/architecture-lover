3e4c45ab8466046e3d578773f210337d24fe9211
Ula <ukazmierczyk@gmail.com>
2017-08-11 18:04:17 +0200

    mobile menu

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -14,4 +14,20 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
   }
 
+  showMenu(){
+    const menuButton = document.querySelector('button.mobile-menu');
+    const menu = document.querySelector('ul');
+
+    if (menu.className !== 'mobile') {
+      menu.setAttribute('class', 'mobile');
+      menu.style.animation = 'slide 0.5s';
+    }
+    else {
+      menu.style.animation = 'slideOut 1s';
+      setTimeout(
+        function() {
+          menu.classList.remove('mobile');
+        }, 500);
+    }
+  }
 }
